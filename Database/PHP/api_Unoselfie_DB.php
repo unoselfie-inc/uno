8be2ad41617b69879cec4f38a40a684112b4e766
kedh <Eric@Ericui-MacBook-Pro.local>
2015-06-13 03:56:37 -0400

    .

--- Database/PHP/api_Unoselfie_DB.php
+++ Database/PHP/api_Unoselfie_DB.php
@@ -10,9 +10,25 @@ class api_Unoselfie_DB{
         $this->username = $username;
         $this->password = $password;
         $this->conn = new mysqli($localhost, $username, $password);
+        if ($this->conn->connect_error) {
+	        die("Connection failed: " . $conn->connect_error);
+	    }
+	    $this->conn->query("Use Unoselfie;");
 	}
 	function __destruct(){
 		$this->conn->close();
+	}
+
+	public function queryUserByUser_id($User_id){
+		/*$stmt = $this->conn->prepare("SELECT * FROM User WHERE User_id=(?)");
+		if (!$stmt->bind_param("i", $User_id)) {
+			echo "Binding parameters failed: (" . $stmt->errno . ") " . $stmt->error;
+		}
+
+		if (!$stmt->execute()) {
+			echo "Execute failed: (" . $stmt->errno . ") " . $stmt->error;
+		}
+		$stmt->close();*/
 	}
 }
 ?>
